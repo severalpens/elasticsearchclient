1386f014517f7f0760c6114f07ad1156e47047e5
Paul Collins <paul.collins@severalpens.com>
2025-02-06 13:58:31 +1100

    update

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,17 +1,25 @@
-interface Faq {
-  _id: string;
-  Question: string;
-  Answer: string;
+interface Faq     {
+    _index: string;
+    _id: number;
+    _score: number;
+    _ignored: Array<string>;
+    _source: {
+        Question: string;
+        Answer: string; 
+    };
 }
-
 export default async function Page() {
     const data = await fetch('https://esresearch.azurewebsites.net/faqs')
     const faqs: Faq[] = await data.json()
     return (
-      <ul>
-        {faqs.map((faq) => (
-          <li key={faq._id}>{faq.Question}</li>
-        ))}
-      </ul>
+        
+      <div>
+            <h1>FAQs</h1>
+          <ul>
+            {faqs.map((faq) => (
+              <li key={faq._id}>{faq._source.Question}</li>
+            ))}
+          </ul>
+      </div>
     )
   }
