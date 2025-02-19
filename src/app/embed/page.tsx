b7c107fe6fdb0aba39e5fde9d09be6128f04bc97
Paul Collins <paul.collins@severalpens.com>
2025-02-19 15:25:52 +1100

    Remove unused variables from embed page component

--- src/app/embed/page.tsx
+++ src/app/embed/page.tsx
@@ -1,10 +1,6 @@
-import Link from "next/link";
 
 
 export default async function Page() {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://esresearch.azurewebsites.net';
-  const indexName = 'faqs';
-  const embeddingType = 'standard';
   return (
     <div>
     </div>
